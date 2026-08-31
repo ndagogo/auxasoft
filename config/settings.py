f45b6262d98b6d80454713b3685ae1142aed4186
ndagogo <124788666+ndagogo@users.noreply.github.com>
2026-08-31 03:32:57 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
     host.strip()
     for host in os.environ.get(
         "DJANGO_ALLOWED_HOSTS",
-        "localhost,127.0.0.1,testserver",
+        "localhost,127.0.0.1,https://auxanosoft.com",
     ).split(",")
     if host.strip()
 ]
