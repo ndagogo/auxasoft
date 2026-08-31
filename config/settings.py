ce59f293a7d0d4898608d6c049283e9bf81fe04c
ndagogo <124788666+ndagogo@users.noreply.github.com>
2026-08-31 04:08:13 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -268,5 +268,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
